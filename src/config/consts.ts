054fb3a9edf191aa93db154dc909763344dee3bb
IvanVidenovic1988 <mailivan019@gmail.com>
2023-04-20 19:34:32 +0200

    request function sorted and fixed

--- src/config/consts.ts
+++ src/config/consts.ts
@@ -1,3 +1,5 @@
-export const API_URL = 'https://sports-sm-distribution-api.de-2.nsoftcdn.com/api/v1/'
-export const BASE_PARAMS =
-  'dataFormat={"default":"object"}&language={"default":"sr-Latn"}&timezone=Europe/Belgrade&companyUuid=4f54c6aa-82a9-475d-bf0e-dc02ded89225'
+export const BASE_CONFIG = {
+  dataFormat: { default: 'object' },
+  language: { default: 'sr-Latn' },
+  timezone: 'Europe/Belgrade',
+}
--- src/shared/utils/request.ts
+++ src/shared/utils/request.ts
@@ -1,7 +1,45 @@
-import { API_URL, BASE_PARAMS } from '../../config/consts'
+import { env } from '../../config/config'
+import { BASE_CONFIG } from '../../config/consts'
+import merge from 'ts-deepmerge'
 
-export const request = async <T>(url: string, params = ''): Promise<T> => {
-  const response = await fetch(`${API_URL}${url}?${BASE_PARAMS}&${params}`)
+type ConfigKey = 'dataFormat' | 'language' | 'timezone'
+type Config = { [key in ConfigKey]?: unknown }
+type Filters = { [key: string]: string | number }
+type Params = {
+  config?: Config
+  filters?: Filters
+}
+
+const prepareConfig = (config?: Config) => {
+  if (!config) return ''
+  return Object.entries(config).reduce((acc, [key, val], index) => {
+    const separator = index === 0 ? '' : '&'
+    return `${acc}${separator}${key}=${JSON.stringify(val)}`
+  }, '')
+}
+
+const prepareFilters = (filters?: Filters) => {
+  if (!filters) return ''
+  return Object.entries(filters).reduce((acc, [key, val]) => {
+    return `${acc}&filter[${key}]=${val}`
+  }, '')
+}
+
+const prepareParams = (params?: Params) => {
+  const mergedConf = merge(params?.config || {}, BASE_CONFIG)
+  const config = prepareConfig(mergedConf)
+  const filters = prepareFilters(params?.filters)
+
+  return config + filters
+}
+
+export const request = async <T>(route: string, params?: Params): Promise<T> => {
+  const queryParams = prepareParams(params)
+
+  const response = await fetch(
+    `${env.API_URL}${route}?${queryParams}&companyUuid=${env.API_KEY}`,
+  )
+
   const { data } = await response.json()
 
   return data as T
